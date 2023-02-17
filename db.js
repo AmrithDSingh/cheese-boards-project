6fa2df6dd96f60621a3371a8e8b605066fa44246
AmrithDSingh <amrithdathsingh04@gmail.com>
2023-02-17 16:58:30 +0000

    tried adding moddels

--- db.js
+++ db.js
@@ -1,9 +1,20 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize, STRING, NUMBER } = require('sequelize');
 
 const sequelize = new Sequelize({
   dialect: 'sqlite',
   storage: './db.sqlite'
 });
-
-
+const newUser = new sequelize.create({
+    name: STRING,
+    email: STRING
+})
+const newBoard = new sequelize.create({
+    type: STRING,
+    description: STRING,
+    rating: NUMBER
+})
+const cheese = new sequelize.create({
+    title: STRING,
+    description: STRING
+})
 module.exports = sequelize;
